f787bd310147d8155dae467cc506042400b2ae4d
Govind Singh Rawat <gokadroid>
2020-10-27 21:53:19 -0500

    9 easy

--- leetcode/easy_set/template.py
+++ leetcode/easy_set/template.py
@@ -2,51 +2,10 @@
 #Question#
 ##########
 '''
-Given a 32-bit signed integer, reverse digits of an integer.
-
-Note:
-Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
-
- 
-
-Example 1:
-
-Input: x = 123
-Output: 321
-Example 2:
-
-Input: x = -123
-Output: -321
-Example 3:
-
-Input: x = 120
-Output: 21
-Example 4:
-
-Input: x = 0
-Output: 0
- 
-
-Constraints:
-
--2**31 <= x <= 2**31 - 1
 '''
 
 
 ##########
 #Solution#
 ##########
-class Solution:
-    def reverse(self, x: int) -> int:
-        negative=False
-        if x<0:
-            x*=-1
-            negative=True
-        
-        num = str(x)[::-1]
-        
-        if negative:
-            return 0 if int(num)*-1 < -2**31 else int(num)*-1
-        else:
-            return 0 if int(num) > (2**31 -1) else int(num)
         
